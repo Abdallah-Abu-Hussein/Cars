509c6c1e0c75b5edf97ca2b7b64068e0c986b388
abdallh1133224 <>
2021-07-14 15:07:18 +0000

    fixing js

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
-// == value 
-//=== value and type
-alert("welcome to my website "+ name)
-var intrested = prompt("Hi, are you intrested in cars ?, type y for yes  or n for no  ")
-if(intrested = 'y'){
+alert("Welcome To My Website "+ name)
+var intrested = prompt("Hi, are you intrested in cars ? Type y for Yes  or n for No  ")
+if(intrested == 'y'){
   alert("you are in the right palce !!")
-}else{
-  alert("sorry to waste your time ): ")
 }
-var intrested2 = prompt("do want to know fun facts about cars ?, type y for yes  or n for no")
+else if (intrested == 'n'){
+  alert("sorry to waste your time ")
+}
+
+var intrested2 = prompt("do want to see the first car in history ? type y for yes or n for No")
 
 
 if(intrested2 = 'y'){
-  document.write('<p>'+ About 165,000 cars are produced every day.
- 
-The new car smell is produced by over 50 volatile organic compounds.
- 
-It would take less than 6 months to get to the moon by car at 60mph.
- 
-Up to 19 girls can be crammed into a Smart car.+'</p>')
-}else{
-  alert("sorry to waste your time ): ")
+document.write(
+  "<div>" +"<h3>" + 'First car in history' + "</h3>"
+ +"<img  src=https://upload.wikimedia.org/wikipedia/commons/thumb/a/a0/Capture_d%E2%80%99%C3%A9cran_2016-10-14_%C3%A0_21.26.28.png/330px-Capture_d%E2%80%99%C3%A9cran_2016-10-14_%C3%A0_21.26.28.png />" + "</div>")
+}
+else{
+ alert("sorry to waste your time")
 }
