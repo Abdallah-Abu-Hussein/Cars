3182a53de6c1eaa3125083339d5cd3c64e552cde
abdallh1133224 <>
2021-07-14 10:36:56 +0000

    fixing html addding js file too

---
+++ app.js
@@ -0,0 +1,12 @@
+var tt = "fghj"
+console.log(tt)
+var z 
+var t = z+10
+console.log(z)
+// == value 
+//=== value and type
+var name = prompt("what is your name  ?")
+alert("welcome to my html "+ name)
+var favcar = prompt("what's your favorit car")
+
+document.write('<p>'+favcar+'</p>')
