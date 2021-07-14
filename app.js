199cb5df57b15f7e25e16a176eb1182fa61f7b03
abdallh1133224 <>
2021-07-14 13:36:52 +0000

    adding more js

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-var tt = "fghj"
-console.log(tt)
-var z 
-var t = z+10
-console.log(z)
 // == value 
 //=== value and type
-var name = prompt("what is your name  ?")
-alert("welcome to my html "+ name)
-var favcar = prompt("what's your favorit car")
+alert("welcome to my website "+ name)
+var intrested = prompt("Hi, are you intrested in cars ?, type y for yes  or n for no  ")
+if(intrested = 'y'){
+  alert("you are in the right palce !!")
+}else{
+  alert("sorry to waste your time ): ")
+}
+var intrested2 = prompt("do want to know fun facts about cars ?, type y for yes  or n for no")
 
-document.write('<p>'+favcar+'</p>')
+
+if(intrested2 = 'y'){
+  document.write('<p>'+ About 165,000 cars are produced every day.
+ 
+The new car smell is produced by over 50 volatile organic compounds.
+ 
+It would take less than 6 months to get to the moon by car at 60mph.
+ 
+Up to 19 girls can be crammed into a Smart car.+'</p>')
+}else{
+  alert("sorry to waste your time ): ")
+}
